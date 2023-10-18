b9fc4805504962939c8b34711b3fe3b15af7d218
romdelaporte <rodrigod@delaportedigital.com>
2023-10-18 14:24:26 -0300

    Some fixes

--- src/components/blog.js
+++ src/components/blog.js
@@ -19,9 +19,9 @@ export default function BlogList() {
     <div className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl text-center">
-          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">From the blog</h2>
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">My blog</h2>
           <p className="mt-2 text-lg leading-8 text-gray-600">
-            Learn how to grow your business with our expert advice.
+            Here you'll find my latest posts.
           </p>
         </div>
         <div className="mx-auto mt-16 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
@@ -50,7 +50,7 @@ export default function BlogList() {
             </div>
             <h3 className="mt-3 text-lg font-semibold leading-6 text-white">
               <span className="absolute inset-0" />
-                  {posts.title}
+                {posts.title}
               {/* <a href="#">
                 
               </a> */}
